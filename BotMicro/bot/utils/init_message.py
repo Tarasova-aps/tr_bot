77f0db71d62b21e1b7fb4ec5a63d9fac7cfccf32
mamsdeveloper <butvin.mihail@yandex.ru>
2023-04-16 09:27:22 +0300

    add edit_or_send function

--- BotMicro/bot/utils/init_message.py
+++ BotMicro/bot/utils/init_message.py
@@ -74,3 +74,37 @@ async def resend_init_message(
             pass
 
     return init_message
+
+
+async def edit_or_resend_init_message(
+    message: Message,
+    bot: Bot,
+    state: FSMContext,
+    text: str,
+    reply_markup: Optional[InlineKeyboardMarkup] = None
+) -> Optional[Message]:
+    data = await state.get_data()
+    init_message_id = data.get('init_message_id')
+
+    if init_message_id is None:
+        init_message = await message.answer(
+            text=text,
+            reply_markup=reply_markup
+        )
+        await state.update_data(init_message_id=init_message.message_id)
+    else:
+        try:
+            init_message = await bot.edit_message_text(
+                chat_id=message.chat.id,
+                message_id=init_message_id,
+                text=text,
+                reply_markup=reply_markup
+            )
+        except TelegramBadRequest:
+            init_message = await message.answer(
+                text=text,
+                reply_markup=reply_markup
+            )
+            await state.update_data(init_message_id=init_message.message_id)
+
+    return init_message if isinstance(init_message, Message) else None
