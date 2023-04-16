0471dcfda788f6f273dff3820cb6d74f2f503bd3
mamsdeveloper <butvin.mihail@yandex.ru>
2023-04-16 07:42:50 +0300

    complete login dialog

--- BotMicro/bot/handlers/admin.py
+++ BotMicro/bot/handlers/admin.py
@@ -2,10 +2,13 @@ from aiogram import F, Bot, Router
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
-from bot.messages.admin import ASK_ACCESS_KEY
+from bot.keyboards.menu import open_menu_btns
+from bot.keyboards.utils import kb_from_btns
+from bot.messages.admin import ASK_ACCESS_KEY, GREET_ADMIN, INCORRECT_ACCESS_KEY
 from bot.states.admin import LoginState
 
 from bot.utils.init_message import edit_init_message, resend_init_message
+from models.user import User
 
 router = Router()
 
@@ -16,14 +19,27 @@ async def admin_handler(message: Message, bot: Bot, state: FSMContext):
     await resend_init_message(
         message, bot, state,
         text=ASK_ACCESS_KEY
-    )    
+    )
     await state.set_state(LoginState.access_key)
 
 
-@router.message(LoginState.access_key, F.text)
-async def access_key_handler(message: Message, bot: Bot, state: FSMContext):
+@router.message(LoginState.access_key, F.text, F.text.as_('text'))
+async def access_key_handler(message: Message, text: str, bot: Bot, state: FSMContext):
     await message.delete()
+    user = await User.get_or_none(text)
+    if not user:
+        await edit_init_message(
+            message, bot, state,
+            text=INCORRECT_ACCESS_KEY
+        )
+        return
+
+    user.chat_id = message.chat.id
+    await user.save()  # type: ignore
+
     await edit_init_message(
         message, bot, state,
-        text='Ваш ключ доступа: {}'.format(message.text)
+        text=GREET_ADMIN(user.name),
+        reply_markup=kb_from_btns(open_menu_btns(user.user_key))
     )
+    await state.clear()
--- BotMicro/bot/messages/admin.py
+++ BotMicro/bot/messages/admin.py
@@ -1,3 +1,12 @@
 ASK_ACCESS_KEY = '''
 Введите ключ доступа:
 '''
+
+INCORRECT_ACCESS_KEY = '''
+Неверный ключ доступа.
+Попробуйте еще раз:
+'''
+
+
+def GREET_ADMIN(name: str):
+    return f'''Добро пожаловать, {name}!'''
