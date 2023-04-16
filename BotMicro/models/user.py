79dccbc82e00c0eaae793f6cae643a70ef318fb7
mamsdeveloper <butvin.mihail@yandex.ru>
2023-04-16 09:26:57 +0300

    add User.deleted field

--- BotMicro/models/user.py
+++ BotMicro/models/user.py
@@ -1,12 +1,19 @@
 from typing import Optional
+
 from odetam.async_model import AsyncDetaModel
 
 
 class User(AsyncDetaModel):
     name: str
+
     chat_id: Optional[int] = None
+
+    deleted: bool = False
+
+    @classmethod
+    async def get_available(cls) -> list['User']:
+        return await User.query(User.deleted == False)  # type: ignore
 
     @property
     def user_key(self) -> str:
         return str(self.key)
-    
