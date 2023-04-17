a8fc936a568c64a445d8bd57111e7650b6a42174
mamsdeveloper <butvin.mihail@yandex.ru>
2023-04-17 07:45:37 +0300

    add Application model

---
+++ BotMicro/models/application.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+from typing import Any
+
+from odetam.async_model import AsyncDetaModel
+from pydantic import Field
+
+
+class Application(AsyncDetaModel):
+    created_at: datetime = Field(default_factory=datetime.now)
+
+    creator_user_id: int
+
+    data: dict[str, Any]
+
+    files: list[tuple[str, str]] # drive, file_name
+    
+    @property
+    def app_key(self) -> str:
+        return str(self.key)
