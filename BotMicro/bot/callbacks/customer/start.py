3f1aeed381ef88ead8e4c2c708cff36fe1565428
mamsdeveloper <butvin.mihail@yandex.ru>
2023-04-17 09:50:21 +0300

    update start, now only greeting

--- BotMicro/bot/callbacks/customer/start.py
+++
@@ -1,9 +0,0 @@
-from aiogram.filters.callback_data import CallbackData
-
-
-class OpenAboutUsCallback(CallbackData, prefix='open_about_us'):
-    pass
-
-
-class OpenContactsCallback(CallbackData, prefix='open_contacts'):
-    pass
--- BotMicro/bot/handlers/customer/start.py
+++ BotMicro/bot/handlers/customer/start.py
@@ -1,15 +1,12 @@
 from aiogram import Bot, Router
 from aiogram.filters import CommandStart
 from aiogram.fsm.context import FSMContext
-from aiogram.types import CallbackQuery, Message
+from aiogram.types import Message
 
-from bot.callbacks.customer.start import (OpenAboutUsCallback,
-                                          OpenContactsCallback)
 from bot.keyboards.customer.menu import open_menu_btns
-from bot.keyboards.customer.start import open_about_us_btns, open_contacts_btns
 from bot.keyboards.utils import kb_from_btns
-from bot.messages.customer.start import ABOUT_US, CONTACTS, GREETING
-from bot.utils.init_message import edit_or_resend_init_message, update_init_message
+from bot.messages.customer.start import GREETING
+from bot.utils.init_message import edit_or_resend_init_message
 
 router = Router()
 
@@ -22,27 +19,5 @@ async def start_handler(message: Message, bot: Bot, state: FSMContext):
     await edit_or_resend_init_message(
         message, bot, state,
         text=GREETING,
-        reply_markup=kb_from_btns(open_about_us_btns())
-    )
-
-
-@router.callback_query(OpenAboutUsCallback.filter())
-async def open_about_us_handler(query: CallbackQuery, message: Message, state: FSMContext):
-    await query.answer()
-
-    await update_init_message(
-        message, state,
-        text=ABOUT_US,
-        reply_markup=kb_from_btns(open_contacts_btns())
-    )
-
-
-@router.callback_query(OpenContactsCallback.filter())
-async def open_contacts_handler(query: CallbackQuery, message: Message, state: FSMContext):
-    await query.answer()
-
-    await update_init_message(
-        message, state,
-        text=CONTACTS,
         reply_markup=kb_from_btns(open_menu_btns())
     )
