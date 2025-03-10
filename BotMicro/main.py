3a214c9d6b0a72785c0ef154a6af2f8666f9dc6e
Tarasova-aps <delaalex@mail.ru>
2025-03-10 18:12:21 +0400

    Update main.py

--- BotMicro/main.py
+++ BotMicro/main.py
@@ -5,8 +5,7 @@ from deta import Deta
 from bot.factory import create_bot, create_dispatcher
 from web.factory import create_app
 
-BOT_TOKEN='6271780599:AAEV_zB7To_jQLdhxbPA6wKP01pzVOB6xZQ' 
-BOT_TOKEN = getenv('6271780599:AAEV_zB7To_jQLdhxbPA6wKP01pzVOB6xZQ')
+BOT_TOKEN = getenv('BIT_TOKEN')
 assert BOT_TOKEN
 
 
